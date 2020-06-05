4f9e4a174db8f526e8a60b39a61e96618786f1a0
yynn-123 <55397241+yynn-123@users.noreply.github.com>
2020-06-05 10:31:12 +0800

    热门课程推荐

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -7,8 +7,9 @@ from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
 
 from apps.courses.models import Course
 
+
 class CourseListView(View):
-    def get(self,request,*args,**kwargs):
+    def get(self, request, *args, **kwargs):
         """
         获取课程列表信息
         :param request:
@@ -17,18 +18,27 @@ class CourseListView(View):
         :return:
         """
         all_courses = Course.objects.all().order_by('-add_time')
-
-
-
+        # 最热门和参与人数排序
+        sort = request.GET.get('sort', '')
+        if sort == 'hot':
+            # 最热门
+            all_courses = all_courses.order_by('-click_nums')
+        elif sort == 'students':
+            # 参与人数
+            all_courses = all_courses.order_by('-students')
+        # 热门课程推荐
+        hot_courses = all_courses.order_by('-click_nums')[:3]
         # 课程机构分页
         try:
             page = request.GET.get('page', 1)
         except PageNotAnInteger:
             page = 1
-        p = Paginator(all_courses, per_page=5, request=request)
+        p = Paginator(all_courses, per_page=3, request=request)
         courses = p.page(page)
 
-        return render(request,'course-list.html',{
-            'all_courses':courses,
+        return render(request, 'course-list.html', {
+            'all_courses': courses,
+            'sort': sort,
+            'hot_courses':hot_courses
 
         })
